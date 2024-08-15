8616dc35244195d2f592a235f6817876b9f40bf6
qdow <156013833+qdow@users.noreply.github.com>
2024-08-15 15:39:24 -0600

    fixed typo in decimal time page

--- src/pages/DecimalTimePage.jsx
+++ src/pages/DecimalTimePage.jsx
@@ -6,7 +6,7 @@ function DecimalTimePage({ time }) {
             <h1>— decimal time —</h1>
             <div className="time-blocks"> <DecimalTime time={time} /> </div>
             <p>Decimal Time is the hour, minute, and second represented as a fraction of their total count.</p>
-            <p>So for example, 2:30am would be 2/60 = 0.03 for the hour, 30/60 = 0.50 for the minute, with the total time of 0.03:0.50 (plus the seconds but I think you get the idea)</p>
+            <p>So for example, 2:30am would be 2/24 = 0.08 for the hour, 30/60 = 0.50 for the minute, with the total time of 0.08:0.50 (plus the seconds but I think you get the idea)</p>
         </div>
     )
 }
